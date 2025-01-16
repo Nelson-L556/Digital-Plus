11572ec61b7c1f3b8066b0c58f6cef9f133b25b3
Nelson Lara <nelson.lara556@hotmail.com>
2025-01-15 20:04:20 -0600

    Se añadio interactividad al menú

--- app.js
+++ app.js
@@ -1,9 +1,58 @@
-const btnServicios = document.querySelectorAll(".bento__btn");
+//* Variabeles
+const iconoCerrar = document.querySelector(".icono__cerrar");
+const navegacionBusqueda = document.querySelector(".navegacion__iconos");
+const fondoBusqueda = document.querySelector(".navegacion__fondo");
+const fondoSearch = document.querySelector(".form");
+const menu = document.querySelector(".navegacion");
+const inputBusqueda = document.querySelector(".navegacion__search")
+const iconoBuscar = document.querySelector(".icono__busqueda");
+const busquedabtn = document.querySelector(".navegacion__btn");
 
-// Agregamos un listener a cada botón
-btnServicios.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault(); // Evita el comportamiento predeterminado del enlace
-        console.log(`Has hecho clic en: ${btn.parentElement.querySelector(".bento__titulo").textContent}`);
-    });
-});
+//* Eventos
+iconoBuscar.addEventListener("click", () =>{
+    abrirMenuBusqueda();
+})
+
+iconoCerrar.addEventListener("click", () => {
+    cerrarMenuBusqueda();
+})
+
+busquedabtn.addEventListener("click", () => {
+    leerInput();
+})
+
+inputBusqueda.addEventListener("input", () => {
+    generarValorBtn();
+})
+
+
+//* Funciones
+function abrirMenuBusqueda() {
+    navegacionBusqueda.classList.add("fondo");
+    iconoCerrar.classList.remove("oculto");
+    fondoBusqueda.classList.add("visible");
+    fondoSearch.classList.add("visible");
+    iconoBuscar.classList.add("oculto")
+    navegacionBusqueda.classList.add("f-start")
+    iconoCarrito.classList.add("oculto")
+}
+
+function cerrarMenuBusqueda() {
+    iconoCerrar.classList.add("oculto")
+    iconoBuscar.classList.remove("oculto")
+    navegacionBusqueda.classList.remove("fondo");
+    fondoBusqueda.classList.remove("visible");
+    fondoSearch.classList.remove("visible");
+    navegacionBusqueda.classList.remove("f-start")
+    iconoCarrito.classList.remove("oculto")
+}
+
+function leerInput(){
+    let valor = inputBusqueda.value;
+    return valor;
+}
+
+function generarValorBtn() {
+    let valorBtn = leerInput();
+    busquedabtn.textContent = `Buscar: "${valorBtn}"`;
+}
